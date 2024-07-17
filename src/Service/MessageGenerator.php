77f0c246079836b0472ff1d0a9b904b4706f28c5
ibrhmkurt <ibrahimkurt@protonmail.com>
2024-07-17 19:16:34 +0300

    how to use multi-service

--- src/Service/MessageGenerator.php
+++ src/Service/MessageGenerator.php
@@ -2,22 +2,36 @@
 
 namespace App\Service;
 
+use Symfony\Component\HttpFoundation\RequestStack;
+
 class MessageGenerator
 {
     /**
      * @var NameGenarator
      */
-
     private $nameGenerator;
 
-    public function __construct(NameGenerator $nameGenerator)
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
+    public function __construct(NameGenerator $nameGenerator, RequestStack $requestStack)
     {
         $this->nameGenerator = $nameGenerator;
+        $this->requestStack = $requestStack;
     }
 
     public function helloMessage()
     {
-        $message = 'Hello '.$this->nameGenerator->randomName().'!';
+        // --->> /hello?name=(write a name)
+        $name = $this->requestStack->getCurrentRequest()->get('name');
+        
+        if(empty($name))
+        {
+            $name = $this->nameGenerator->randomName();
+        }
+        $message = 'Hello '.$name.'!';
 
         return $message;
     }
