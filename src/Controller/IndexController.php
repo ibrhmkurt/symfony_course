dbc82b45b8c601d8a46704116c13da16f0f90de7
ibrhmkurt <ibrahimkurt@protonmail.com>
2024-07-20 13:01:35 +0300

    requests

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,5 +16,34 @@ class IndexController extends AbstractController
     public function index(): Response
     {
         return new JsonResponse(['message' => 'Merhaba Dünya!']);
+    }
+
+    /**
+     * @Route("/request", name="request_test")
+     * @param RequestStack $requestStack
+     */
+    public function requestTest(RequestStack $requesStack): Response
+    {
+        $request = $requesStack->getCurrentRequest();
+
+        // $_POST 
+        $name = $request->request->get('name');
+
+        // $_GET
+        $request->query->get('name');
+
+        // $_COOKİE
+        $request->cookies->get('username');
+
+        // php de karşılığı yok
+        $request->attributes->get('name');
+
+        // $_FILE
+        $request->files->get('filename');
+
+        // $_SERVER
+        $serverData = $request->server->all();
+
+        var_dump($serverData);exit();
     }
 }
